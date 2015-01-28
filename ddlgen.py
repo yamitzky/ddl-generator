1509a4cf739230913fd629c8dd3fb84194f7caa8
Mitsuki Ogasahara <ogasahara_mitsuki@cyberagent.co.jp>
2015-01-28 10:18:57 +0900

    Fix: mysql params can be specified in default way

--- ddlgen.py
+++ ddlgen.py
@@ -8,16 +8,27 @@ password = sys.argv[2]
 db = sys.argv[3]
 
 def execute(command):
-    return commands.getoutput('echo "%s" | mysql -u %s --password=%s %s' % (command, user, password, db)).split("\n")
+    return commands.getoutput('echo "%s" | mysql %s' % (command, " ".join(sys.argv[1:]))).split("\n")
+
+def discard(rows):
+    discarded = False
+    new_rows = []
+    for row in rows:
+        if discarded:
+            new_rows.append(row)
+        else:
+            if row.startswith("********"):
+                discarded = True
+    return new_rows
 
 for table in execute("SHOW TABLES")[1:]:
-    res = execute("SHOW CREATE TABLE \`%s\` \G" % table)
-    if len(res) >= 3:
-        if res[2].strip().startswith("View"):
-            rows = res[3].split(":", 1)[-1].replace(",", ",\n").split("\n")
+    res = discard(execute("SHOW CREATE TABLE \`%s\` \G" % table))
+    if len(res) >= 2:
+        if res[0].strip().startswith("View"):
+            rows = res[1].split(":", 1)[-1].replace(",", ",\n").split("\n")
             cols = ["\t" + row.strip() + "," for row in rows]
         else:
-            cols = ["\t" + row.strip() for row in res[3:]]
+            cols = ["\t" + row.strip() for row in res[1:]]
 
         print table
         print "\n".join(cols)
